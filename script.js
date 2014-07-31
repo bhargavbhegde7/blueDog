274f404e9468950250a2a3c9f7c39a9b7a3a171b
BHARGAV <bhargavbhegde7@gmail.com>
2014-07-31 21:07:25 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+			$('#read1').addClass('animated fadeInLeft');
+			$('.heading').addClass('animated rotateInDownLeft');
+			$('.heading_shadow').addClass('animated rotateInDownLeft');
+			$('.quote').addClass('animated rotateInDownRight');
+			$('.quote2').addClass('animated rotateInDownRight');
+
+		});
