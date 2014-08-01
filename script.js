c8a911f4923e38cff39d9c99cbf2e312bd903852
bhargav <bhargavbhegde7@gmail.com>
2014-08-01 11:45:42 +0530

    new changes2

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
-			$('#read1').addClass('animated fadeInLeft');
+			$('#read1').addClass('animated rotateInDownLeft');
+			$('#read2').addClass('animated rotateInDownRight');
+
+			$('#write1').addClass('animated rotateInUpLeft');
+			$('#write2').addClass('animated rotateInUpRight');
+
 			$('.heading').addClass('animated rotateInDownLeft');
 			$('.heading_shadow').addClass('animated rotateInDownLeft');
 			$('.quote').addClass('animated rotateInDownRight');
 			$('.quote2').addClass('animated rotateInDownRight');
-
-		});
+});
