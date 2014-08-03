e7ca2911608267b653f0a06485e3842c4c2b97ce
bhargav <bhargavbhegde7@gmail.com>
2014-08-03 22:47:04 +0530

    validated js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+			//$('#sform').validate();
 			$('#read1').addClass('animated rotateInDownLeft');
 			$('#read2').addClass('animated rotateInDownRight');
 
@@ -9,4 +10,44 @@ $( document ).ready(function() {
 			$('.heading_shadow').addClass('animated rotateInDownLeft');
 			$('.quote').addClass('animated rotateInDownRight');
 			$('.quote2').addClass('animated rotateInDownRight');
+
+			/*$('#uname').bind('blur',function(event){
+				//$('#login_error').text('error');
+			});*/
+			/*$('#sform').submit(function(){
+				$('#login_error').text('error');
+			});*/
+			$('#button_sub').bind('click',function(){
+				//$('#sform').validate();
+				//alert();
+				//document.submit();
+				
+				var uname = $('#sform').find('input[name="uname"]').val();
+				var pwd = $('#sform').find('input[name="pwd"]').val();
+
+				if(!uname||!pwd) //document.submit();
+				{
+					$('#login_error').text('testing the validation.... are we?');
+				}
+				else
+				{
+					$('#login_error').text('');
+					//alert("all ok");
+					$('#sform').submit();
+				}
+			});
+
 });
+/*
+function submited()
+{
+	var uname = document.getElementById("uname").value;
+	var pwd = document.getElementById("pwd").value;
+	//alert(uname+" "+pwd);
+	if(!uname)
+	{
+		$('#login_error').text('error');
+	}
+	//document.submit();
+}
+*/
